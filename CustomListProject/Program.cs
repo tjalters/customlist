06f8dba7deb25419d346dd3c78f3254afb2ecfb7
tjalters <tjalters32@gmail.com>
2017-03-06 10:23:57 -0600

    deleted notes

--- CustomListProject/Program.cs
+++ CustomListProject/Program.cs
@@ -10,12 +10,7 @@ namespace CustomListProject
     {
         static void Main(string[] args)
         {
-            //List<int> numbers = new List<int>();
-            //List<string> strings = new List<string>();
-            //CustomList<int> myListOne = new CustomList<int>();
-            //CustomList<int> myListTwo = new CustomList<int>();
-            //CustomList<string> stringListOne = new CustomList<string>();
-            //CustomList<string> stringListTwo = new CustomList<string>();
+            
             CustomList<int> list = new CustomList<int>();
             
         }
